815a8891e289edab9139eb7b2892dd13aa60491a
Krzysztof Findeisen <kfindeis@uw.edu>
2020-04-08 19:32:37 -0500

    Fix bug blocking defect ingestion.

--- config/datasetIngest.py
+++ config/datasetIngest.py
@@ -4,3 +4,6 @@ config.refcats = {
     'gaia': 'gaia_cosmos_2016.tar.gz',
     'panstarrs': 'panstarrs_cosmos_2016.tar.gz'
 }
+
+# Workaround for DM-24395
+config.defectIngester.register.visit = ['calibDate', 'filter']
