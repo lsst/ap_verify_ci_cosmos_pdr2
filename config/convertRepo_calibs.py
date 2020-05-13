42516f4f62eebd4ab83d01790135a4e7aee5f58f
Krzysztof Findeisen <kfindeis@uw.edu>
2020-05-13 15:18:02 -0500

    Fix redundant line in convertRepo configs.

--- config/convertRepo_calibs.py
+++ config/convertRepo_calibs.py
@@ -1,6 +1,5 @@
 # Config overrides for convert_gen2_repo_to_gen3.py
 
-config.datasetIgnorePatterns.append("raw")
 config.datasetIgnorePatterns = ["raw", "*Coadd_skyMap", "ref_cat", "defects"]
 # Suppress auto-conversion of skymaps; it causes trouble for the template repo
 config.rootSkyMapName = None
