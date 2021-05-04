91e0a00cfbcd35e33523090c40e6bb7bfffbfa4e
Meredith Rawls <mrawls@uw.edu>
2021-05-03 21:36:45 -0500

    Turn brighter-fatter on in isr config to match gen3 default

--- config/isr.py
+++ config/isr.py
@@ -1,7 +1,6 @@
 # Config override for lsst.ip.isr.IsrTask
 
 # Can't ingest the necessary datasets
-config.doBrighterFatter = False
 config.doAttachTransmissionCurve = False
 config.doUseOpticsTransmission = False
 config.doUseFilterTransmission = False
