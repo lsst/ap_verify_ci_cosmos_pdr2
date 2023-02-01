2bb1d76de8782536ca60f08b828cdc4118dbd5a7
Krzysztof Findeisen <kfindeis@uw.edu>
2023-02-01 11:30:54 -0800

    Fix deprecation warning from calibrate config.
    The Gen 2 refcat config is no longer required, and will break the
    build after Pipelines release 26.

--- config/calibrate.py
+++ config/calibrate.py
@@ -8,10 +8,8 @@ hscDir = os.path.join(getPackageDir("obs_subaru"), "config")
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth).
 config.connections.astromRefCat = "gaia"
-config.astromRefObjLoader.ref_dataset_name = config.connections.astromRefCat
 config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
 config.astromRefObjLoader.filterMap = {}
 
 # Use panstarrs for photometry (grizy filters).
 config.connections.photoRefCat = "panstarrs"
-config.photoRefObjLoader.ref_dataset_name = config.connections.photoRefCat
