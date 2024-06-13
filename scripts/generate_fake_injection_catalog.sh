d087682d37083aa0039bb85bb7386d760f2e93a2
Bruno Sanchez <bsanchez@cppm.in2p3.fr>
2024-06-13 20:31:01 +0200

    Adding fake injection catalog to preloaded repo

---
+++ scripts/generate_fake_injection_catalog.sh
@@ -0,0 +1,68 @@
+SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
+REPO_DIR="${SCRIPT_DIR}/../preloaded/"
+
+print_error() {
+    >&2 echo "$@"
+}
+
+usage() {
+    print_error
+    print_error "Usage: $0 [-b BUTLER_REPO] -c ROOT_COLLECTION [-h]"
+    print_error
+    print_error "Specific options:"
+    print_error "   -b          Butler repo yaml file URI, defaults to preloaded repo"
+    print_error "   -o          name of the output collection for injection catalog"
+    print_error "   -h          show this message"
+    exit 1
+}
+
+parse_args() {
+    while getopts "b:o:h" option $@; do
+        case "$option" in
+            b)  BUTLER_REPO="$OPTARG";;
+            o)  OUTPUT_COLLECTION="$OPTARG";;
+            h)  usage;;
+            *)  usage;;
+        esac
+    done
+    if [[ -z "${BUTLER_REPO}" ]]; then
+        BUTLER_REPO=$REPO_DIR
+    fi
+    if [[ -z "${OUTPUT_COLLECTION}" ]]; then
+        print_error "$0: mandatory argument -- o"
+        usage
+        exit 1
+    fi
+}
+parse_args $@
+
+generate_injection_catalog \
+    -a 149.3 151.2 \
+    -d 1.4 2.95 \
+    -m 21 28 \
+    -i g r i \
+    -p source_type Star \
+    -s 5500 \
+    -b ${BUTLER_REPO} \
+    -w goodSeeingCoadd \
+    -c 'templates/goodSeeing' \
+    -o ${OUTPUT_COLLECTION} \
+    -m 18 26 \
+    -i g \
+    --seed 314 
+
+
+
+# generate_injection_catalog \
+#     -a 149.3 151.2 \
+#     -d 1.4 2.95 \
+#     -m 21 28 \
+#     -i g r i \
+#     -p source_type Star \
+#     -s 5500 \
+#     -b preloaded2 \
+#     -w goodSeeingCoadd \
+#     -c templates/goodSeeing \
+#     -o fake_injection_catalog \
+#     -m 18 26.5 \
+#     --seed 314 
